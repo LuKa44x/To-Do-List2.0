ee0e19e85eb7710b730ad66de8fe9aac43886a40
LuKa44x <lucavicari44@gmail.com>
2025-07-11 12:23:54 +0200

    Little fix
    Formatted document, removed an empty function in todoManager.js and removed the useless 'export' in setBoolTrue in projectsManager.js.

--- src/projectsManager.js
+++ src/projectsManager.js
@@ -7,57 +7,57 @@ const project = {
     todo: []
 }
 
-export function addProject(){
+export function addProject() {
 
-    
-        let projectName 
-        while (projectName===undefined|| projectName===null) projectName= prompt('Enter project name:');
-        project.name=projectName;
-        projects.push({ ...project });
-        console.log(projectName + "==Nome progetto");
-        console.log(projects);
-        return projectName;
-        
+
+    let projectName
+    while (projectName === undefined || projectName === null) projectName = prompt('Enter project name:');
+    project.name = projectName;
+    projects.push({ ...project });
+    console.log(projectName + "==Nome progetto");
+    console.log(projects);
+    return projectName;
+
 }
 
 export function selectProject(nProject) {
     setBoolTrue();
-    
-    const selectedProject= projects.find(e => e.name === nProject.textContent); 
-            console.log(selectedProject);         //selezione del corretto project in projects
-        console.log("Progetto "+ selectedProject.name + " selezionato."); 
-        console.log(projects);
-        return selectedProject.name;
-        
-        
- 
-    
-    
+
+    const selectedProject = projects.find(e => e.name === nProject.textContent);
+    console.log(selectedProject);         //selezione del corretto project in projects
+    console.log("Progetto " + selectedProject.name + " selezionato.");
+    console.log(projects);
+    return selectedProject.name;
+
+
+
+
+
 }
 
 export function addTodoToProject(todoItem, selectedProjectName) {
     projects.forEach(e => {
-       
+
         if (e.name === selectedProjectName) {
-        e.todo=todoItem; 
+            e.todo = todoItem;
         }
     })
-    
+
 }
 
-export function deleteProject(idName){
+export function deleteProject(idName) {
     let index;
-   
-        
-            index=projects.findIndex(e => e.name ===idName);
-            projects.splice(index,1);
-     
+
+
+    index = projects.findIndex(e => e.name === idName);
+    projects.splice(index, 1);
+
 }
 
-export function setBoolTrue() {
+function setBoolTrue() {
     selectBool = true;
 }
 
-export function getBool(){
+export function getBool() {
     return selectBool;
 }
--- src/todoManager.js
+++ src/todoManager.js
@@ -1,4 +1,4 @@
-import {getBool} from "./projectsManager";
+import { getBool } from "./projectsManager";
 
 const todoItem = {
     name: '',
@@ -7,18 +7,15 @@ const todoItem = {
     priority: ''
 }
 
-export function addTodo(){
-    if(getBool()){
-    todoItem.name = prompt('Enter todo name:');
-    todoItem.description = prompt('Enter todo description:');
-    todoItem.dueDate = prompt('Enter todo due date (YYYY-MM-DD):');
-    todoItem.priority = prompt('Enter todo priority (Low, Medium, High):');
-    
-    console.log(todoItem);
-    return todoItem;
+export function addTodo() {
+    if (getBool()) {
+        todoItem.name = prompt('Enter todo name:');
+        todoItem.description = prompt('Enter todo description:');
+        todoItem.dueDate = prompt('Enter todo due date (YYYY-MM-DD):');
+        todoItem.priority = prompt('Enter todo priority (Low, Medium, High):');
+
+        console.log(todoItem);
+        return todoItem;
     } else return alert("Seleziona il progetto prima.");
 }
 
-export function showTodo(){
-   
-}
